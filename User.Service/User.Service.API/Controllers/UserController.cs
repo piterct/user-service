9777d4bb6867253094bb1727c2db42e19475f331
Michael Peter <michael_piterct@hotmail.com>
2021-08-11 22:50:53 -0300

    ILogger

--- User.Service/User.Service.API/Controllers/UserController.cs
+++ User.Service/User.Service.API/Controllers/UserController.cs
@@ -1,6 +1,17 @@
-﻿namespace User.Service.API.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace User.Service.API.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class UserController
     {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
     }
 }
